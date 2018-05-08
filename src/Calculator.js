efa96067469ac6d2b3dcab65e7b1aead9bdcbe6f
Donato Azevedo <donatoaz@gmail.com>
2018-05-08 10:54:47 -0300

    Adds Enzyme and simple static rendered mkup tests

--- src/Calculator.js
+++ src/Calculator.js
@@ -14,7 +14,23 @@ class Calculator extends Component {
 
     return (
       <div className="calculator">
-        {displayValue}
+        <div className="display">
+          {displayValue}
+        </div>
+        <div className="digitButton">0</div>
+        <div className="digitButton">1</div>
+        <div className="digitButton">2</div>
+        <div className="digitButton">3</div>
+        <div className="digitButton">4</div>
+        <div className="digitButton">5</div>
+        <div className="digitButton">6</div>
+        <div className="digitButton">7</div>
+        <div className="digitButton">8</div>
+        <div className="digitButton">9</div>
+        <div className="operationButton">+</div>
+        <div className="operationButton">-</div>
+        <div className="operationButton">\/</div>
+        <div className="operationButton">x</div>
       </div>
     );
   }
--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Calculator from './Calculator';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Calculator />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('HTML', () => {
+  it('calculator should have a display div', () => {
+    const wrapper = render(<Calculator />);
+    expect(wrapper.find('.display').length).toEqual(1);
+  });
+
+  it('calculator should have 10 digit keys', () => {
+    const wrapper = render(<Calculator />);
+    expect(wrapper.find('.digitButton').length).toEqual(10);
+  });
+
+  it('calculator should have 4 operation keys', () => {
+    const wrapper = render(<Calculator />);
+    expect(wrapper.find('.operationButton').length).toEqual(4);
+  });
 });
 
 it('adds numbers', () => {
