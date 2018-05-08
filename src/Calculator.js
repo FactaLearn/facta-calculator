55261209a1e12530c96fde3a1dad4dbf449159ff
Facta <facta@Mac-mini-de-Facta-2.local>
2018-05-08 16:55:20 -0300

    calculator add

--- src/Calculator.js
+++ src/Calculator.js
@@ -17,20 +17,45 @@ class Calculator extends Component {
         <div className="display">
           {displayValue}
         </div>
-        <div className="digitButton">0</div>
-        <div className="digitButton">1</div>
-        <div className="digitButton">2</div>
-        <div className="digitButton">3</div>
-        <div className="digitButton">4</div>
-        <div className="digitButton">5</div>
-        <div className="digitButton">6</div>
-        <div className="digitButton">7</div>
-        <div className="digitButton">8</div>
-        <div className="digitButton">9</div>
-        <div className="operationButton">+</div>
-        <div className="operationButton">-</div>
-        <div className="operationButton">\/</div>
-        <div className="operationButton">x</div>
+
+        <div className="firstRow">
+          <div className="topButton">AC</div>
+          <div className="topButton">+/-</div>
+          <div className="topButton">%</div>
+        </div>
+
+        <div className="secondRow">
+          <div className="digitButton">7</div>
+          <div className="digitButton">8</div>
+          <div className="digitButton">9</div>
+          <div className="operationButton">/</div>
+        </div>
+
+        <div className="thirdRow">
+          <div className="digitButton">4</div>
+          <div className="digitButton">5</div>
+          <div className="digitButton">6</div>
+        </div>
+
+        <div className="rightRow">
+          <div className="operationButton">/</div>
+          <div className="operationButton">x</div>
+          <div className="operationButton">-</div>
+          <div className="operationButton">+</div>
+          <div className="operationButton">=</div>
+        </div>
+
+        <div className="fourthRow">
+          <div className="digitButton">1</div>
+          <div className="digitButton">2</div>
+          <div className="digitButton">3</div>
+        </div>
+
+        <div className="lastRow">
+          <div className="doubleButton">0</div>
+          <div className="digitButton">,</div>
+        </div>
+
       </div>
     );
   }
