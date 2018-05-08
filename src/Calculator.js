4a19fd5b4ed8ee1f8b26b8f8db78a988c8b11e1a
Facta <facta@Mac-mini-de-Facta-2.local>
2018-05-08 11:22:01 -0300

    finished

--- src/Calculator.js
+++ src/Calculator.js
@@ -13,8 +13,42 @@ class Calculator extends Component {
     const { displayValue } = this.state;
 
     return (
-      <div className="calculator">
-        {displayValue}
+      <div>
+      <table>
+            <tr>
+              <th colspan="4" className="calculator" style ={{textAlign: "right"}}> {displayValue}</th>
+            </tr>
+            <tr>
+                <td className="top">AC</td>
+                <td className="top">+ -</td>
+                <td className="top">%</td>
+                <td className="right">/</td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                <td className="right">X</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td className="right">-</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td className="right">+</td>
+            </tr>
+            <tr>
+                <td colspan="2" className="left">0</td>
+                <td>,</td>
+                <td className="right">=</td>
+            </tr>
+        </table>
+       
       </div>
     );
   }
