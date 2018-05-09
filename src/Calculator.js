be3bc390df57769c1b8ee4e3b3400738d8d3ca95
Henrique Batista <henrique@factati.com>
2018-05-09 09:52:11 -0300

    ultimas alteracoes

--- src/Calculator.js
+++ src/Calculator.js
@@ -19,25 +19,32 @@ class Calculator extends Component {
         </div>
 
         <div className="container">
-          <div className="topButton">AC</div>
-          <div className="topButton">+/-</div>
-          <div className="topButton">%</div>
-          <div className="rightButton">/</div>
-          <div className="digitButton">7</div>
-          <div className="digitButton">8</div>
-          <div className="digitButton">9</div>
-          <div className="rightButton">x</div>
-          <div className="digitButton">4</div>
-          <div className="digitButton">5</div>
-          <div className="digitButton">6</div>
-          <div className="rightButton">-</div>
-          <div className="digitButton">1</div>
-          <div className="digitButton">2</div>
-          <div className="digitButton">3</div>
-          <div className="rightButton">+</div>
+          <div className="firstColumn">AC</div>
+          <div className="secondColumn">+/-</div>
+          <div className="thirdColumn">%</div>
+          <div className="fourthColumn">➗</div>
+
+
+
+          <div className="firstColumn">7</div>
+          <div className="secondColumn">8</div>
+          <div className="thirdColumn">9</div>
+          <div className="fourthColumn">x</div>
+
+
+          <div className="firstColumn">4</div>
+          <div className="secondColumn">5</div>
+          <div className="thirdColumn">6</div>
+          <div className="fourthColumn">-</div>
+
+          <div className="firstColumn">1</div>
+          <div className="secondColumn">2</div>
+          <div className="thirdColumn">3</div>
+          <div className="fourthColumn">+</div>
+
           <div className="doubleButton">0</div>
-          <div className="digitButton">,</div>
-          <div className="rightButton">=</div>
+          <div className="thirdColumn">,</div>
+          <div className="fourthColumn">=</div>
         </div>
 
 
