91dbf9f93c07de8674d9a3bc724ae1a353e124d4
Facta <facta@Mac-mini-de-Facta-2.local>
2018-05-09 12:20:44 -0300

    grid modified to flex

--- src/Calculator.js
+++ src/Calculator.js
@@ -21,26 +21,32 @@ class Calculator extends Component {
           <div className="function">AC</div>
           <div className="function">±</div>
           <div className="function">%</div>
-        </div>
-        <div className="operators">
           <div className="operator">/</div>
-          <div className="operator">x</div>
-          <div className="operator">-</div>
-          <div className="operator">+</div>
-          <div className="operator lower-right-corner">=</div>
         </div>
-        <div className="numbers">
+        <div className="first">
           <div className="number">7</div>
           <div className="number">8</div>
           <div className="number">9</div>
+          <div className="operator">x</div>
+        </div>
+        <div className="second">
           <div className="number">4</div>
           <div className="number inner">5</div>
           <div className="number">6</div>
+          <div className="operator">-</div>
+        </div>
+        <div className="third">
           <div className="number">1</div>
           <div className="number">2</div>
           <div className="number">3</div>
-          <div className="number double lower-left-corner">0</div>
+          <div className="operator">+</div>
+        </div>
+        <div className="everything">
+          <div className="number double lower-left-corner">
+          <span>0</span>
+          </div>
           <div className="number">,</div>
+          <div className="operator lower-right-corner">=</div>
         </div>
       </div>
     );
